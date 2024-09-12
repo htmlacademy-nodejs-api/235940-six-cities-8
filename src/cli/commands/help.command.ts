24f99d9b54ec559c430c05d4fb13b30ae4b7fb0d
Artur Samoilov <artsamoilov@gmail.com>
2024-09-12 03:26:05 +0200

    feat: adds colors to help command response text

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { ICommand } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements ICommand {
   public getName(): string {
@@ -7,16 +8,16 @@ export class HelpCommand implements ICommand {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-      Программа для подготовки данных для REST API сервера.
+      ${chalk.bold.inverse(' Программа для подготовки данных для REST API сервера. ')}
 
-      Пример: cli.js --<command> [--arguments]
+      ${chalk.italic('Пример:')} ${chalk.whiteBright('cli.js')} ${chalk.cyanBright('--<command>')} ${chalk.yellowBright('[--arguments]')}
 
-      Команды:
+      ${chalk.italic('Команды:')}
+       ${chalk.cyanBright('--version:')}                     ${chalk.italic.dim('# выводит номер версии')}
+       ${chalk.cyanBright('--help:')}                        ${chalk.italic.dim('# печатает этот текст')}
+       ${chalk.cyanBright('--import')} ${chalk.yellowBright('<path>')}:               ${chalk.italic.dim('# импортирует данные из TSV')}
+       ${chalk.cyanBright('--generate')} ${chalk.yellowBright('<n> <path> <url>')}:   ${chalk.italic.dim('# генерирует произвольное количество тестовых данных')}
 
-       --version:                   # выводит номер версии
-       --help:                      # печатает этот текст
-       --import <path>:             # импортирует данные из TSV
-       --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
     `);
   }
 }
